fefead2a50b3412ee79b9920b017b86a612b52e6
Baptiste <chene.baptiste@hotmail.fr>
2019-06-26 17:55:06 +0200

    jeu du pendu finalisé

---
+++ js/mots.js
@@ -0,0 +1,8 @@
+function mots_a_trouver()
+{
+	return 	str ="Oiseau : Quel mot de 6 lettres comporte 5 voyelles différentes ?*Peigne : Il a des dents mais ne mord pas.*Guerre : Avant de l'avoir,on ne la veut pas,mais quand on l'a,on ne veut pas la perdre*Crayon : Je parle toutes les langues et j'ai tout le temps la tête à l'envers.*Secret : Quand je suis découvert je n'existe plus.*Volcan : Je suis de terre, de feu et de pierre. Je me réveille pour troubler les sommeils. Et je me rendors jusqu'à ma prochaine effusion d'or.*Langue : Je suis toujours à l'abri mais pourtant je suis toujours mouillée, qui suis-je ?*Oppose : -2 est L'------ de 2*Soleil : En me levant je ne fais pas de bruit mais je réveille tout le monde. Qui suis-je ?*Mousse : Je suis souvent présent sur l'eau, mais je peux également l'être dans les bois, je peux être bue ou mangée, qui suis je ?";
+
+}
+
+
+
